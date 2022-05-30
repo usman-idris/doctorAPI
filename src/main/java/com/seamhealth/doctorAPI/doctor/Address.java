2c9522b1395fe7fee187da34c0d8f50643c929c7
Usman Idris Kwaru <kwaru91@gmail.com>
2022-05-30 10:31:28 +0100

    first commit

---
+++ src/main/java/com/seamhealth/doctorAPI/doctor/Address.java
@@ -0,0 +1,9 @@
+package com.seamhealth.doctorAPI.doctor;
+
+public class Address {
+    private Long id;
+    private Doctor doctor;
+    private String homeAddress;
+    private String lGA;
+    private String state;
+}
---
+++ src/main/java/com/seamhealth/doctorAPI/doctor/Doctor.java
@@ -0,0 +1,90 @@
+package com.seamhealth.doctorAPI.doctor;
+
+public class Doctor {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNo;
+
+    public Doctor(Long id, String firstName, String lastName, String email, String phoneNo) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return String return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the phoneNo
+     */
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    /**
+     * @param phoneNo the phoneNo to set
+     */
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+}
